83f71c8dbe5232c53556ef899b903506da63e66c
James Howell <47699550+james-howell0@users.noreply.github.com>
2022-08-02 14:56:50 +0100

    Add files via upload

---
+++ pullIntunePackage.ps1
@@ -0,0 +1,18 @@
+﻿while ($true){
+    Get-ChildItem -Path "C:\Windows\IMECache\" | ForEach-Object {
+        if (([System.IO.FileInfo]$_.FullName).LastWriteTime.Date -ge [datetime]::Today ){
+            $folderName = ([System.IO.Path]::GetFileNameWithoutExtension($(Split-Path -Path $_.FullName -Leaf)))
+            break
+        }    
+    }
+}
+$outputFolder = "$env:ProgramData\HCC\Temp\$folderName"
+New-Item -ItemType Directory -Path $outputFolder -Force 
+while ($true){
+    Get-ChildItem -Path "C:\Windows\IMECache\" | ForEach-Object {
+        if (([System.IO.FileInfo]$_.FullName).LastWriteTime.Date -ge [datetime]::Today ){     
+            Get-ChildItem -Path $_.FullName | ForEach-Object {Copy-Item -Path $_.FullName -Destination $outputFolder -Recurse -Force -ErrorAction SilentlyContinue}
+            break
+        }
+    }
+}
