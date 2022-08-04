d01d1d437b58fc297d2a158f225d1e6a84bba262
James Howell <47699550+james-howell0@users.noreply.github.com>
2022-08-04 08:39:50 +0100

    Add files via upload

---
+++ clearWindowsUpdateFolder.ps1
@@ -0,0 +1,8 @@
+﻿Stop-Service -Name "Windows Update" -Force
+if (Test-Path -Path "C:\Windows\SoftwareDistribution.old") {
+    Remove-Item -Path "C:\Windows\SoftwareDistribution.old" -Force
+    Rename-Item -Path "C:\Windows\SoftwareDistribution" -NewName "C:\Windows\SoftwareDistribution.old" -Force -ErrorAction SilentlyContinue
+} else {
+    Rename-Item -Path "C:\Windows\SoftwareDistribution" -NewName "C:\Windows\SoftwareDistribution.old" -Force -ErrorAction SilentlyContinue
+}
+Start-Service -Name "Windows Update"
